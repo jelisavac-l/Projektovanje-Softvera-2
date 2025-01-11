cef674ffa675f14c939bbeac4597912c2d3a411e
jelisavac-l <jelisavacluka63@gmail.com>
2025-01-11 03:18:45 +0100

    Od intellijevog gui alata gori video nisam.

---
+++ Client/src/main/java/client/Client.java
@@ -0,0 +1,11 @@
+package client;
+
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+public class Client {
+
+    public static void main(String[] args) {
+        FlatMacLightLaf.setup();
+
+    }
+}
