0a2c29b9408a504f2ae1a198fb3947293899e0ad
jelisavac-l <jelisavacluka63@gmail.com>
2025-08-11 02:31:14 +0200

    Podrska za ubacivanje stavki kontrola.

--- Server/src/main/java/operations/evaluation/EvaluationCreation.java
+++ Server/src/main/java/operations/evaluation/EvaluationCreation.java
@@ -1,17 +1,27 @@
 package operations.evaluation;
 
 import domain.DomainObject;
+import domain.Evaluation;
+import domain.EvaluationItem;
 import operations.Operation;
+
+import java.util.List;
 
 public class EvaluationCreation extends Operation {
 
     @Override
     public boolean checkConstraints(DomainObject domainObject) {
-        return true;
+        return domainObject instanceof Evaluation;
     }
 
     @Override
     public boolean executeOperation(DomainObject domainObject) {
-        return databaseBroker.createRecord(domainObject);
+        Evaluation evaluation = (Evaluation) domainObject;
+        var signal = databaseBroker.createRecord(evaluation);
+        List<EvaluationItem> items = evaluation.getItems();
+        for (EvaluationItem item : items) {
+            signal = signal && databaseBroker.createRecord(item);
+        }
+        return signal;
     }
 }
